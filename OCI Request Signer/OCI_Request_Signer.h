f979f3e57c90a070a7a08b78859faa42d9d182a7
Scott Harwell <scott@harwell.me>
2019-09-04 15:04:06 -0400

    Initial Commit

---
+++ OCI Request Signer/OCI_Request_Signer.h
@@ -0,0 +1,19 @@
+//
+//  OCI_Request_Signer.h
+//  OCI Request Signer
+//
+//  Created by Scott Harwell on 9/4/19.
+//  Copyright © 2019 Scott Harwell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OCI_Request_Signer.
+FOUNDATION_EXPORT double OCI_Request_SignerVersionNumber;
+
+//! Project version string for OCI_Request_Signer.
+FOUNDATION_EXPORT const unsigned char OCI_Request_SignerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCI_Request_Signer/PublicHeader.h>
+
+
