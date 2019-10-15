847be9219bc215d2e32bb40bcfe4c63520db4aba
Scott Harwell <scott@harwell.me>
2019-10-15 15:56:10 -0400

    Correct Path

--- Package.swift
+++ Package.swift
@@ -25,9 +25,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Swift OCI Request Signer",
-            dependencies: []),
+            dependencies: [],
+            path: "OCIRequestSigner"),
         .testTarget(
-            name: "Swift OCI Request SignerTests",
-            dependencies: ["Swift OCI Request Signer"]),
+            name: "Swift OCI Request Signer Tests",
+            dependencies: ["Swift OCI Request Signer"],
+            path: "OciRequestSignerTests"),
     ]
 )
