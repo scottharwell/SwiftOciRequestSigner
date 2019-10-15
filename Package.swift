d1eacd558c82de09de2f90fc55fa57c74409ec27
Scott Harwell <scott@harwell.me>
2019-10-15 17:16:30 -0400

    Dependency correction

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .target(
             name: "OciRequestSigner",
             dependencies: [
-                "BlueRSA",
+                "CryptorRSA",
                 "CryptoSwift"
             ],
             path: "OCIRequestSigner"),
